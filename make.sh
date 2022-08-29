73a0284211ebefdf0da1e67996e38279ba6f4ed0
adamzvolanek <waddoo123+github@gmail.com>
2022-08-28 20:24:05 -0500

    updated make.sh script, includes cleanup!

--- make.sh
+++ make.sh
@@ -6,5 +6,11 @@
 # From:
 # https://jupyterbook.org/en/stable/sphinx/index.html
 
+# Make it
+jupyter-book build lecturebook --all --path-output _build
+
 # Publish it
 ghp-import -p -b gh-pages lecturebook/_build/html
+
+# Clean Up
+jupyter-book clean -a lecturebook
